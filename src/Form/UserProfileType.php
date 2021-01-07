639fbba2047e48e5eeccb8cd4f7fa2c2b2f14e56
Dovydas <dodefke@gmail.com>
2021-01-07 23:20:46 +0200

    minor bug fixes

--- src/Form/UserProfileType.php
+++ src/Form/UserProfileType.php
@@ -7,15 +7,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserProfile1Type extends AbstractType
+class UserProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
             ->add('surname')
-            ->add('age')
-        ;
+            ->add('age');
     }
 
     public function configureOptions(OptionsResolver $resolver)
