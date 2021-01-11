50472af42ca58411595197319c2f06e0f391eef0
Dovydas <dodefke@gmail.com>
2021-01-11 17:22:03 +0200

    Added auth and login form

---
+++ src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UsersFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'the_new_password'
+        ));
+    }
+}
