689db9972c95b25af29f088df87d17c23bb00e79
Dovydas <dodefke@gmail.com>
2021-01-15 10:18:15 +0000

    Added test file and installed xdebugger

--- tests/Controller/ContactControllerTest.php
+++ tests/Controller/ContactControllerTest.php
@@ -10,8 +10,23 @@ class ContactControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $client->request('GET', '/post/hello-world');
+        $crawler = $client->request('GET', '/contact');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->filter('form')->form();
+
+        $form->setValues(['contact[fullName]' => 'Dovydas', 'contact[email]' => 'test@test.ru', 'contact[message]' => 'TEST']);
+
+        $crawler = $client->submit($form);
+
+        $this->assertEmailCount(1);
+
+        $email = $this->getMailerMessage(0);
+
+        $this->assertEmailHeaderSame($email, 'From', 'info@info.info');
+        $this->assertEmailHeaderSame($email, 'To', 'test@test.ru');
+
+        $this->assertEmailTextBodyContains($email, 'TEST');
     }
 }
